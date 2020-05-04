56f9fc7fc9d867e08db5332928049697bdfb718b
Lukas Chripko <lukas@healthplus.ai>
2020-05-04 12:58:53 +0200

    Update pulsar to load service url from ENV

--- __init__.py
+++ __init__.py
@@ -1,12 +1,25 @@
 import io
+import os
 import pulsar
 import _pulsar
 import fastavro
-import fastavroparsing
+from fhirfastavro import avroutil
 from pulsar.schema import Schema
+
+pulsar_address = os.environ.get("PULSAR_ADDRESS")
+
+if not pulsar_address:
+    print('Pulsar address not set')
+    exit(1)
 
 
 class AvroSchema(Schema):
+    # Custom AvroSchema for pulsar, unfortunatelly pulsar AvroSchema does not
+    # support namespaced schemas spread accross multiple files
+    # and expects one big schema file
+
+    # This a mix of BytesSchema and original AvroSchema
+
     def __init__(self, record_cls=None, schema_definition=None,
                  schema_name=None):
         super().__init__(record_cls, _pulsar.SchemaType.BYTES,
@@ -28,13 +41,12 @@ class AvroSchema(Schema):
         return
 
 
-service_url = 'pulsar://10.164.15.212:6650'
-
+service_url = f"pulsar://{pulsar_address}:6650"
 client = pulsar.Client(service_url)
 
-fastavro_schema = fastavroparsing.get_bundle_schema()
+fastavro_schema = avroutil.get_bundle_schema()
 fastavro_schema = AvroSchema(schema_definition=fastavro_schema,
-                             schema_name=fastavro_schema['name'])
+                                 schema_name=fastavro_schema['name'])
 
 
 def publisher(topic_name, schema):
@@ -62,8 +74,10 @@ def subscribe(topic_name, subscription_name, schema, callback,
 
     client.close()
 
+
 def callback_info(message):
     return message.value(), message.properties()
+
 
 def close():
     client.close()
